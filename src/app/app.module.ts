56b3c9eed35ec9896cf9828c9adf37ea0707ab53
My Name <Vsadnick95@yandex.ru>
2021-06-20 15:56:43 +0300

    create folder app/ngrx/characterslist with fails:characters-list.action, characters-list.reduser; create folder app/ngrx/characterlist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { StoreModule } from '@ngrx/store';
-import * as CharacterRedusers from './characters.reduser';
+import * as CharacterRedusers from './ngrx/characterslist/characters-list.reduser';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
@@ -23,7 +27,10 @@ import * as CharacterRedusers from './characters.reduser';
     BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
-    StoreModule.forRoot({character: CharacterRedusers.reduser})
+    StoreModule.forRoot({characterList: CharacterRedusers.reduser}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
